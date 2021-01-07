0554817716c316673f60c794ae5b70bfc37723ee
Rodrigo dos Santos Felix <43767651+felixrodrigo19@users.noreply.github.com>
2021-01-06 21:36:59 -0300

    Create read_FITS_file.py

---
+++ read_FITS_file.py
@@ -0,0 +1,26 @@
+from astropy.io import fits
+# Write your load_fits function here.
+def load_fits(fit):
+  arq = fits.open(fit)
+  return arq.info()
+
+
+
+if __name__ == '__main__':
+  # Run your `load_fits` function with examples:
+  bright = load_fits('image0.fits')
+  print(bright)
+
+  # You can also confirm your result visually:
+  from astropy.io import fits
+  import matplotlib.pyplot as plt
+
+  hdulist = fits.open('image1.fits')
+  data = hdulist[0].data
+
+  # Plot the 2D image data
+  plt.imshow(data.T, cmap=plt.cm.viridis)
+  plt.colorbar()
+  plt.show()
+
+ 
